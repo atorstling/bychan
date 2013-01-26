3d43ed5ec45b11dc74d9a3d21e3ce4b1ceb32e3a
Alexander Torstling <alexander@torstling.com>
2013-01-26 18:02:32 +0100

    interactive

--- src/test/java/com/torstling/tdop/CompleteTest.java
+++ src/test/java/com/torstling/tdop/CompleteTest.java
@@ -3,6 +3,10 @@ package com.torstling.tdop;
 import com.torstling.tdop.CalculatorParser;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import static org.junit.Assert.assertEquals;
 
 public class CompleteTest {
@@ -15,5 +19,20 @@ public class CompleteTest {
         SubtractionNode secondLeft = new SubtractionNode(new DigitNode(1), new DigitNode(2));
         assertEquals(new SubtractionNode(new MultiplicationNode(secondLeft, secondRight), topRight), rootNode);
         assertEquals(rootNode.evaluate(), -7);
+    }
+
+    @Test
+    public void test2() {
+        Node rootNode = new CalculatorParser().parse("1-2*3");
+        assertEquals(rootNode.evaluate(), -5);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to calculator");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(new CalculatorParser().parse(line).evaluate());
+        }
     }
 }
