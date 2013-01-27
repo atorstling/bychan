b46845000bae9e0f08ab0310250240a57e18e1e5
Alexander Torstling <alexander@torstling.com>
2013-01-27 13:54:48 +0100

    comments

--- src/main/java/com/torstling/tdop/CalculatorParserImpl.java
+++ src/main/java/com/torstling/tdop/CalculatorParserImpl.java
@@ -21,11 +21,26 @@ public class CalculatorParserImpl implements TokenParserCallback {
 
     @NotNull
     public Node expression(int callerInfixBindingPower) {
-        Node currentRoot = tokens.pop().prefixParse(this);
+        //An expression always starts with a symbol which can qualify as a prefix value
+        //i.e
+        // "+" as in "positive", used in for instance "+3 + 5", parses to +(rest of expression)
+        // "-" as in "negative", used in for instance "-3 + 5", parses to -(rest of expression)
+        // "(" as in "start subexpression", used in for instance "(3)", parses rest of expression with 0 strength,
+        //         which keeps going until next 0-valued token is encountered (")" or end)
+        // any digit, used in for instance "3", parses to 3.
+        Node currentLeftHandSide = tokens.pop().prefixParse(this);
+        //When we have the prefix parsing settled, we cannot be sure that the parsing is done. Digit parsing
+        //returns almost immediately for instance. If the prefix parse swallowed all the expression, only the end
+        //token will remain. But since the end token has 0 binding power, we will never continue in this case.
+        //
+        //If there is more to parse, however, we should keep parsing as long as we don't encounter a weaker token
+        //than the caller is currently parsing. When a weaker token is encountered, the tokens so far should namely
+        //be wrapped up into a part-expression.
         while (tokens.peek().infixBindingPower() > callerInfixBindingPower) {
-            currentRoot = tokens.pop().infixParse(currentRoot, this);
+            //Parsing happens by passing the current LHS to the operator, which will continue parsing.
+            currentLeftHandSide = tokens.pop().infixParse(currentLeftHandSide, this);
         }
-        return currentRoot;
+        return currentLeftHandSide;
     }
 
     public void swallow(Class<? extends Token> expectedClass) {
