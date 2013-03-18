bfb674cb69d73980a5e332c3392c7eba2b5dd8a4
Alexander Torstling <alexander@torstling.com>
2013-03-18 17:41:55 +0100

    test

--- src/test/java/com/torstling/tdop/BooleanExpressionParserTest.java
+++ src/test/java/com/torstling/tdop/BooleanExpressionParserTest.java
@@ -2,10 +2,19 @@ package com.torstling.tdop;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class BooleanExpressionParserTest {
 
     @Test
     public void doit() {
-        new Lexer(BooleanExpressionTokens.get());
+        Lexer lexer = new Lexer(BooleanExpressionTokens.get());
+        List<Token> tokenStream = lexer.lex("a | b");
+        assertTrue(tokenStream.get(0) instanceof VariableToken);
+        assertTrue(tokenStream.get(1) instanceof OrToken);
+        assertTrue(tokenStream.get(2) instanceof VariableToken);
+        assertTrue(tokenStream.get(3) instanceof EndToken);
     }
 }
