f38d3decdc1474141e4a0c50ee9cb83c85c697f6
Alexander Torstling <alexander@torstling.com>
2014-12-04 21:16:44 +0100

    Refactor JsonTest

--- src/test/java/com/torstling/tdop/fluid/json/JsonTest.java
+++ src/test/java/com/torstling/tdop/fluid/json/JsonTest.java
@@ -2,6 +2,8 @@ package com.torstling.tdop.fluid.json;
 
 import com.torstling.tdop.fluid.Language;
 import com.torstling.tdop.fluid.LanguageBuilder2;
+import com.torstling.tdop.fluid.LevelLanguageBuilder2;
+import com.torstling.tdop.fluid.WrappedTokenDefinitionBuilder;
 import com.torstling.tdop.fluid.json.nodes.JsonNode;
 import com.torstling.tdop.fluid.json.nodes.StringLiteralNode;
 import org.junit.Test;
@@ -14,21 +16,25 @@ import static org.junit.Assert.assertEquals;
 public class JsonTest {
 
     @Test
-    public void onlyText() {
+    public void simpleStringLiteral() {
         JsonNode ast = parse("\"hello\"");
         assertEquals(new StringLiteralNode("hello"), ast);
     }
 
     private JsonNode parse(String text) {
         LanguageBuilder2<JsonNode> lb = new LanguageBuilder2<>();
-        Language<JsonNode> l = lb.newLevel()
-                .startToken().named("string_literal").matchesPattern("\"(\\w*)\"")
+        LevelLanguageBuilder2<JsonNode> l1 = lb.newLevel();
+        Language<JsonNode> l =
+                stringLiteral(l1).completeLanguage();
+        return l.getParser().parse(text);
+    }
+
+    private WrappedTokenDefinitionBuilder<JsonNode> stringLiteral(LevelLanguageBuilder2<JsonNode> l1) {
+        return l1.startToken().named("string_literal").matchesPattern("\"(\\w*)\"")
                 .standaloneParseAs((previous, match) -> {
                     String withinQuotationMarks = match.group(1);
                     return new StringLiteralNode(withinQuotationMarks);
-                })
-                .completeLanguage();
-        return l.getParser().parse(text);
+                });
     }
 
 }
