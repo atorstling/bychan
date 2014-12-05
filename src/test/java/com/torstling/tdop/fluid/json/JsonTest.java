6c602b68d2f6ab54b8782863ab17c4f4c7187b28
Alexander Torstling <alexander@torstling.com>
2014-12-05 11:24:18 +0100

    Refactor JsonTest

--- src/test/java/com/torstling/tdop/fluid/json/JsonTest.java
+++ src/test/java/com/torstling/tdop/fluid/json/JsonTest.java
@@ -5,6 +5,7 @@ import com.torstling.tdop.fluid.LanguageBuilder2;
 import com.torstling.tdop.fluid.TokenDefinition;
 import com.torstling.tdop.fluid.TokenDefinitionBuilder;
 import com.torstling.tdop.fluid.json.nodes.JsonNode;
+import com.torstling.tdop.fluid.json.nodes.NumberLiteralNode;
 import com.torstling.tdop.fluid.json.nodes.StringLiteralNode;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
@@ -22,6 +23,24 @@ public class JsonTest {
         assertEquals(new StringLiteralNode("hello"), ast);
     }
 
+    @Test
+    public void positiveInteger() {
+        Language<JsonNode> l = new LanguageBuilder2<JsonNode>()
+                .newLevel().addToken(number())
+                .completeLanguage();
+        JsonNode ast = l.getParser().parse("1");
+        assertEquals(new NumberLiteralNode(1), ast);
+    }
+
+    @Test
+    public void negativeExponent() {
+        Language<JsonNode> l = new LanguageBuilder2<JsonNode>()
+                .newLevel().addToken(number())
+                .completeLanguage();
+        JsonNode ast = l.getParser().parse("-0.5e-5");
+        assertEquals(new NumberLiteralNode(-0.5e-5f), ast);
+    }
+
     @NotNull
     private TokenDefinition<JsonNode> stringLiteral() {
         return new TokenDefinitionBuilder<JsonNode>().named("string_literal").matchesPattern("\"(\\w*)\"")
@@ -30,5 +49,12 @@ public class JsonTest {
                     return new StringLiteralNode(withinQuotationMarks);
                 }).build();
     }
+
+    @NotNull
+    private TokenDefinition<JsonNode> number() {
+        return new TokenDefinitionBuilder<JsonNode>().named("number_literal").matchesPattern("-?[0-9]+(\\.[0-9]+)?([eE]([+-])?[0-9]+)?")
+                .standaloneParseAs((previous, match) -> new NumberLiteralNode(Float.valueOf(match.getText()))).build();
+    }
+
 
 }
---
+++ src/test/java/com/torstling/tdop/fluid/json/nodes/NumberLiteralNode.java
@@ -0,0 +1,34 @@
+package com.torstling.tdop.fluid.json.nodes;
+
+/**
+ * Created by alext on 2014-12-05.
+ */
+public class NumberLiteralNode implements JsonNode {
+    private final float f;
+
+    public NumberLiteralNode(float f) {
+        this.f = f;
+    }
+
+    @Override
+    public Object evaluate() {
+        return f;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NumberLiteralNode that = (NumberLiteralNode) o;
+
+        if (Float.compare(that.f, f) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (f != +0.0f ? Float.floatToIntBits(f) : 0);
+    }
+}
