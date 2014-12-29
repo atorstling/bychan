9727f430d3d812bc2cbda6d24ab9f5d97b7a2a1e
Alexander Torstling <alexander@torstling.com>
2014-12-29 15:35:15 +0100

    more PositionTracer tests

--- src/test/java/org/bychan/core/basic/PositionTracerImplTest.java
+++ src/test/java/org/bychan/core/basic/PositionTracerImplTest.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.when;
 public class PositionTracerImplTest {
 
     @Test
-    public void empty() {
+    public void emptyNonePopped() {
         String input = "";
         PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
         TokenStack<Object> stack = new TokenStack<>(mockEndToken(input));
@@ -19,7 +19,16 @@ public class PositionTracerImplTest {
     }
 
     @Test
-    public void beforeStarted() {
+    public void emptyEndPopped() {
+        String input = "";
+        PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
+        TokenStack<Object> stack = new TokenStack<>(mockEndToken(input));
+        stack.pop();
+        assertEquals(new ParsingPosition(new TextPosition(0, 1, 1), stack), t.getParsingPosition(stack));
+    }
+
+    @Test
+    public void singleNonePopped() {
         String input = "a";
         PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
         TokenStack<Object> stack = new TokenStack<>(mockEndToken(input));
@@ -32,20 +41,64 @@ public class PositionTracerImplTest {
     }
 
     @Test
-    public void onePickedUp() {
+    public void singleOnePopped() {
         String input = "a";
         PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
+        Token<Object> first = mockToken(input, 0);
+        TokenStack<Object> stack = new TokenStack<>(first, mockEndToken(input));
+        stack.pop();
+        ParsingPosition parsingPosition = t.getParsingPosition(stack);
+        assertEquals(new ParsingPosition(new TextPosition(0, 1, 1), stack), parsingPosition);
+    }
+
+    @Test
+    public void singleEndPopped() {
+        String input = "a";
+        PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
+        Token<Object> first = mockToken(input, 0);
+        TokenStack<Object> stack = new TokenStack<>(first, mockEndToken(input));
+        stack.pop();
+        stack.pop();
+        ParsingPosition parsingPosition = t.getParsingPosition(stack);
+        assertEquals(new ParsingPosition(new TextPosition(0, 1, 1), stack), parsingPosition);
+    }
+
+    @Test
+    public void dualTwoPopped() {
+        String input = "ab";
+        PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
+        Token<Object> first = mockToken(input, 0);
+        Token<Object> second = mockToken(input, 1);
+        TokenStack<Object> stack = new TokenStack<>(first, second, mockEndToken(input));
+        stack.pop();
+        stack.pop();
+        ParsingPosition parsingPosition = t.getParsingPosition(stack);
+        assertEquals(new ParsingPosition(new TextPosition(1, 1, 2), stack), parsingPosition);
+    }
+
+    @Test
+    public void dualEndPopped() {
+        String input = "ab";
+        PositionTracerImpl<Object> t = new PositionTracerImpl<>(input);
+        Token<Object> first = mockToken(input, 0);
+        Token<Object> second = mockToken(input, 1);
+        TokenStack<Object> stack = new TokenStack<>(first, second, mockEndToken(input));
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        ParsingPosition parsingPosition = t.getParsingPosition(stack);
+        assertEquals(new ParsingPosition(new TextPosition(1, 1, 2), stack), parsingPosition);
+    }
+
+    private Token<Object> mockToken(String input, int startPosition) {
         //noinspection unchecked
         Token<Object> first = mock(Token.class);
         //noinspection unchecked
         TokenType<Object> tokenType = mock(TokenType.class);
         //noinspection unchecked
-        when(first.getMatch()).thenReturn(new LexingMatch<>(0, 1, "a", tokenType));
+        when(first.getMatch()).thenReturn(new LexingMatch<>(startPosition, startPosition+1, input, tokenType));
         when(first.getType()).thenReturn(tokenType);
-        TokenStack<Object> stack = new TokenStack<>(first, mockEndToken(input));
-        stack.pop();
-        ParsingPosition parsingPosition = t.getParsingPosition(stack);
-        assertEquals(new ParsingPosition(new TextPosition(0, 1, 1), stack), parsingPosition);
+        return first;
     }
 
 }
