20754f7efac2366762c4e30c1d600993b122fc30
Alexander Torstling <alexander@torstling.com>
2015-02-18 09:04:51 +0100

    doc

--- src/main/java/org/bychan/core/dynamic/DynamicNudParseAction.java
+++ src/main/java/org/bychan/core/dynamic/DynamicNudParseAction.java
@@ -9,6 +9,18 @@ import org.jetbrains.annotations.Nullable;
  * which is needed in the dynamic environment.
  */
 public interface DynamicNudParseAction<N> {
+    /**
+     * Parse this lexeme as the start of a new block.
+     *
+     * @param left   the result of parsing the lexemes directly preceding this lexeme. This parameter is passed
+     *               in the NUD case even though it is not specified in the original paper. The reason is that
+     *               it might be handy to know the directly preceding result when implementing nested scopes and
+     *               other constructions. Without this knowledge the parser must keep external extra state for these
+     *               things.
+     * @param parser Useful for continuing the parsing
+     * @param lexeme The current lexeme
+     * @return the resulting AST node.
+     */
     @NotNull
     N parse(@Nullable N left, @NotNull UserParserCallback<N> parser, @NotNull Lexeme<N> lexeme);
 }
