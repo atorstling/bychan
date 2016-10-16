aa9badd70376290b445716d12b08320b83ad1d8c
Alexander Torstling <alexander@torstling.com>
2016-10-16 16:26:06 +0200

    doc

--- src/main/java/org/bychan/core/dynamic/DynamicLedParseAction.java
+++ src/main/java/org/bychan/core/dynamic/DynamicLedParseAction.java
@@ -7,8 +7,15 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Similar to {@link org.bychan.core.basic.LedParseAction}, but receives more runtime information
  * which is needed in the dynamic environment.
- *
  */
 public interface DynamicLedParseAction<N> {
+    /**
+     * Parse this lexeme as the continuation of a block.
+     *
+     * @param left   The result of parsing the lexemes directly preceding this lexeme.
+     * @param parser Useful for continuing the parsing
+     * @param lexeme The lexeme which triggered this action
+     * @return the resulting AST node.
+     */
     N parse(@Nullable N left, @NotNull UserParserCallback<N> parser, @NotNull Lexeme<N> lexeme);
 }
